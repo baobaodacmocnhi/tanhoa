b87157ee218ad94c0c057e74d910a2246f5b5b0d
baobaodacmocnhi <baobaodacmocnhi@gmail.com>
2024-01-25 20:27:33 +0700

    update

--- QuaySoMayMan_PC/QuaySoMayMan_PC/frmHienThi.Designer.cs
+++ QuaySoMayMan_PC/QuaySoMayMan_PC/frmHienThi.Designer.cs
@@ -45,7 +45,7 @@
             this.label1.Cursor = System.Windows.Forms.Cursors.Default;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 80.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
             this.label1.ForeColor = System.Drawing.Color.Lime;
-            this.label1.Location = new System.Drawing.Point(502, 230);
+            this.label1.Location = new System.Drawing.Point(500, 340);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(344, 120);
             this.label1.TabIndex = 4;
@@ -61,7 +61,7 @@
             this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 80.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
             this.label2.ForeColor = System.Drawing.Color.Lime;
-            this.label2.Location = new System.Drawing.Point(502, 406);
+            this.label2.Location = new System.Drawing.Point(500, 510);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(344, 120);
             this.label2.TabIndex = 5;
@@ -74,7 +74,7 @@
             this.label3.BackColor = System.Drawing.Color.Transparent;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 80.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
             this.label3.ForeColor = System.Drawing.Color.Lime;
-            this.label3.Location = new System.Drawing.Point(502, 592);
+            this.label3.Location = new System.Drawing.Point(500, 680);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(344, 120);
             this.label3.TabIndex = 6;
@@ -99,7 +99,7 @@
             this.label4.Cursor = System.Windows.Forms.Cursors.Default;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 50.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
             this.label4.ForeColor = System.Drawing.Color.Yellow;
-            this.label4.Location = new System.Drawing.Point(382, 66);
+            this.label4.Location = new System.Drawing.Point(389, 180);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(1140, 76);
             this.label4.TabIndex = 7;
@@ -109,7 +109,7 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(213, 51);
+            this.pictureBox1.Location = new System.Drawing.Point(220, 165);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(120, 120);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -123,7 +123,7 @@
             this.AutoScroll = true;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(1748, 756);
+            this.ClientSize = new System.Drawing.Size(1748, 898);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.txtSTT);
