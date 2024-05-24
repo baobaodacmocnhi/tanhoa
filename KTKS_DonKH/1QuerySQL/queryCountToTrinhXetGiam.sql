ad713a260187d242502b00b026e67240d7752789
baobaodacmocnhi <baobaodacmocnhi@gmail.com>
2024-05-24 21:02:37 +0700

    u

---
+++ KTKS_DonKH/1QuerySQL/queryCountToTrinhXetGiam.sql
@@ -0,0 +1,4 @@
+﻿select * from
+(select ttct.CreateDate,DanhBo,HoTen,DiaChi,VeViec,NoiDung,MaDonMoi,STT from ToTrinh tt,ToTrinh_ChiTiet ttct where VeViec like N'%xét giảm%' and YEAR(ttct.CreateDate)>=2022 and tt.ID=ttct.ID)t1,
+(select MaDonMoi,STT,KyHD,GiaBieu,GiaBieu_BD,DinhMuc,DinhMuc_BD,TieuThu,TieuThu_BD,GiaDieuChinh from DCBD dc,DCBD_ChiTietHoaDon dchd where dc.MaDCBD=dchd.MaDCBD and TieuThu>=200)t2
+where t1.MaDonMoi=t2.MaDonMoi and t1.STT=t2.STT order by t1.CreateDate asc
